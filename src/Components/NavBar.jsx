03244d154b18db094f2223779043389b022b1b38
Hasmik20 <61650766+Hasmik20@users.noreply.github.com>
2021-03-15 19:35:26 +0400

    update post

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import ButtonGroup from './Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { faPhotoVideo, faStickyNote,faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { faPhotoVideo, faStickyNote,faCalendar,faWindowClose } from '@fortawesome/free-solid-svg-icons'
 import InputImg from './InputImg';
+import Example from './Example'
 
 class NavBar extends Component {
- 
+
   render() { 
     return ( 
       <div className="navDiv">
         <InputImg />
         <nav>
           <ul className ="ulClass">
-            <li> Photo 
+            <li><Example /> 
             <FontAwesomeIcon className="marginIcon" icon={faPhotoVideo} />
+          
             </li>
             <li>
-              Add Notes
+            <Example /> 
               <FontAwesomeIcon className="marginIcon" icon={faStickyNote} />
             </li>
             <li>
-              Calendar
+            <Example /> 
               <FontAwesomeIcon className="marginIcon" icon={faCalendar} />
             </li>
           </ul>
         </nav>
-        
-        <ButtonGroup  />
-       
-       
-        
+      
       </div>
      );
   }
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+
+
 import NavBar from './NavBar';
 
 const Components = () => {
   return ( 
     <React.Fragment>
       <NavBar />
-     
     </React.Fragment>
    );
 }
