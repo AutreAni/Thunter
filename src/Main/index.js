c5cb08e28ae364da12311c180b777dbe3a301995
YourName <tadevosyanshushan23@gmail.com>
2021-03-17 11:30:03 +0400

    added new folders commits

--- src/Main/index.js
+++ src/Main/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import SectionProfile from '../UserProfile/SectionProfile/index';
 import Components from '../Components/index';
 import '../css-modules/Main/main.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import HomePage from './componentsS/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+// import './css/styles.scss';
 
 
 const Main = (props) => {
@@ -13,6 +20,9 @@ const Main = (props) => {
             />
             <Components userData = {props.userData}
             />
+            <Provider store={store}>
+              <HomePage />
+            </Provider>
             </div>
                   
         </div>
