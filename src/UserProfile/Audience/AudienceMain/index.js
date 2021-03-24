655d6be3bbd0f105f050fa54e3e27019adcc58d7
Ani <69779787+AutreAni@users.noreply.github.com>
2021-03-24 18:16:04 +0700

    network in audience

--- src/UserProfile/Audience/AudienceMain/index.js
+++ src/UserProfile/Audience/AudienceMain/index.js
@@ -1,26 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AudienceInfo from '../AudienceInfo/index';
 import AudienceList from '../AudienceList/index';
 import '../../../css-modules/UserProfile/Audience/AudienceMain/style.css';
-
+import NetworkIndex from '../../../Network/index';
 
 
 const AudienceMain = ({ userData }) => {
     const [users, setUsers] = useState([]);
 
-    const audience = userData.audience; 
+    const audience = userData.audience;
     let requestUrl = userData.password ?
         `http://localhost:3000/users/` :
         `http://localhost:3000/fbUsers/`;
 
-   useEffect(()=> {
-    audience.forEach(user => {
-        fetch(`${requestUrl}${user}`)
-            .then(response => response.json())
-            .then(data => setUsers(users => [...users, data]))
+    useEffect(() => {
+        if (!users.length) {
+            (audience.forEach(user => {
+                fetch(`${requestUrl}${user}`)
+                    .then(response => response.json())
+                    .then(data => setUsers(users => [...users, data]))
+            }))
+        }
     })
-   }, []);
-    
+
+
+
     return (
         <div className="container">
             <div className="audience__main">
@@ -29,9 +33,10 @@ const AudienceMain = ({ userData }) => {
                         audience={audience}
                     />
                     <AudienceList
-                        users = {users}
+                        users={users}
                     />
                 </div>
+                <NetworkIndex />
             </div>
         </div>
     )
