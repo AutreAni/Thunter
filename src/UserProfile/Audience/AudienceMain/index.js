be1240dd0f23cbfb3a5f1b9e9a95e9415ed19729
Ani <69779787+AutreAni@users.noreply.github.com>
2021-04-06 22:19:24 +0700

    router bugs fixed

--- src/UserProfile/Audience/AudienceMain/index.js
+++ src/UserProfile/Audience/AudienceMain/index.js
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import AudienceInfo from '../AudienceInfo/index';
 import AudienceList from '../AudienceList/index';
 import "../../../css-modules/UserProfile/Audience/AudienceMain/style.css";
 
 const AudienceMain = ({ userData, users, showUserProfile, audienceList, pageToPreview, emptyUserArray }) => {
-   
-    // const [users, setUsers] = useState([]);
-
-    // const audience = userData.audience;
-    // let requestUrl = userData.password ?
-    //     `http://localhost:3000/users/` :
-    //     `http://localhost:3000/fbUsers/`;
-
-
-    // useEffect(() => {
-    //     if (!users.length) {
-    //         (audience.forEach(user => {
-    //             fetch(`${requestUrl}${user}`)
-    //                 .then(response => response.json())
-    //                 .then(data => setUsers(users => [...users, data]))
-    //         }))
-    //     }
-    // })
-
-    // const emptyUserArray = () => {
-    //     setUsers([]);
-    // }
 
     return (
         <div className="details">
