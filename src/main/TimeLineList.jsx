7972809e6ad742f204c6235c23de7593deb9a794
Hasmik20 <61650766+Hasmik20@users.noreply.github.com>
2021-04-08 15:42:15 +0400

    c

--- src/main/TimeLineList.jsx
+++ src/main/TimeLineList.jsx
@@ -3,12 +3,16 @@ import LikeButtons from "../PostTimeline/LikesButtons"
 import InputImg from '../PostTimeline/InputImg';
 
 
+
 const MainList = ({blogs}) =>{
+
+
   return (
     <div style={{width:"600px",margin:"auto"}}>
         {blogs.map(d =>(
           <div key={d.id} className='divContainer'>
              <p style={{fontSize:"12px"}}>{d.date}</p>
+            
             <div style={{marginTop:"-3%"}}> 
                <img src={d.logo} className="logo" / >
                 <a href={d.link}>{d.title}</a> 
