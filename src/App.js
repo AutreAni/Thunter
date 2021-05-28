23d10271bdc795bd9d44db852bf7de00275c02af
AutreAni <autreani@gmail.com>
2021-05-28 17:02:17 +0700

    state with react-redux

--- src/App.js
+++ src/App.js
@@ -5,31 +5,8 @@ import Home from './Home/index';
 import { useSelector } from 'react-redux';
 
 function App() {
-
+  //state removed from app
   const currentUser = useSelector(state => state.currentUser);
-
-
-
-
-  const performSignOut = () => {
-    // setCurrentUser(null);
-  }
-
-  const updatePicture = (field, url) => {
-    // setCurrentUser({
-    //   ...currentUser,
-    //   [field]: url
-    // });
-  }
-
-  const updateUserData = (obj) => {
-    // setCurrentUser({
-    //   ...currentUser,
-    //   ...obj
-    // })
-  }
-
-
   return (
     <div className="App">
       { currentUser ?
